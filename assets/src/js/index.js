19033b39fd8f13ec07375e06b21e26dc101bfc69
Hamed <hamedpyn@gmail.com>
2023-08-16 21:49:40 +0430

    setting the selected img src as the user img

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -15,3 +15,19 @@ let imagesArray = ['assets/Images/rock.png', 'assets/Images/paper.png', 'assets/
 let cpuResult = document.querySelector('#computerField span span');
 // User score element
 let userResult = document.querySelector('#usersField span span');
+
+// Adding click event listeners to game options
+gamesOption.forEach(item => {
+    item.addEventListener('click', (e) => {
+        // Adding 'active' class to the clicked option
+        item.classList.add('active');
+        // Getting the image src of the clicked option
+        let ImagesSrc = e.target.src;
+        // Setting the user's selection field with the clicked image
+        usersField.src = ImagesSrc;
+        // Displaying "Wait..." as the result text
+        resultText.textContent = 'Wait...';
+        // Resetting the result text color
+        resultText.style.color = '';
+    });
+});
