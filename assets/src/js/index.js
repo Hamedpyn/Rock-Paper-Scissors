3197f77b764ecdd05165123327ec51dae2f9def5
Hamed <hamedpyn@gmail.com>
2023-08-16 21:47:47 +0430

    Selecting DOM elements

---
+++ assets/src/js/index.js
@@ -0,0 +1,17 @@
+
+// Selecting DOM Elements
+
+// user selected Image
+let usersField = document.querySelector('#usersField img');
+// computer random selected Image
+let computerField = document.querySelector('#computerField img');
+// result text
+let resultText = document.querySelector('#resultText');
+// Game options elements
+let gamesOption = document.querySelectorAll('.options');
+// Array of available images 
+let imagesArray = ['assets/Images/rock.png', 'assets/Images/paper.png', 'assets/Images/scissors.png'];
+// CPU score element
+let cpuResult = document.querySelector('#computerField span span');
+// User score element
+let userResult = document.querySelector('#usersField span span');
